8c8b20c0134b74bd18be90c09869274d636bfc91
Waqqas Iqbal <46201687+waqqasiq@users.noreply.github.com>
2019-01-05 20:22:24 +0600

    Update devskill_number_base.cpp
    Corrected indentation

--- Devskill/devskill_number_base.cpp
+++ Devskill/devskill_number_base.cpp
@@ -24,55 +24,51 @@ int main() {
 			if(x==0){
 				cout<<0<<endl;
 				flag=1;
-			}
+			} //end if
 			if(flag==0){
-		while(x>0){
-			v.push_back(x%m);
-			x=x/m;
-		}
-		reverse(v.begin(),v.end());
-	//	cout<<n<<" "<<m<<endl;
+				while(x>0){
+					v.push_back(x%m);
+					x=x/m;
+				} //end while
+				reverse(v.begin(),v.end());
 		
-		for(int i=0;i<v.size();i++){
-			cout<<v[i]<<"";
-		}
-		cout<<""<<endl;
-			}
-		}
+				for(int i=0;i<v.size();i++){
+					cout<<v[i]<<"";
+				} // end for
+				cout<<""<<endl;
+			} // end if
+		} // end if
 		else{
 			if(x==0){
 				cout<<0<<endl;
 				flag=1;
-			}
+			} //end if
 			if(flag==0){
-			while(x>0){
-				int y=x%m;
-				x=x/m;
-				if(y>=10){
-					v2.push_back(y);
-				}
-				else{
-					v2.push_back(y);
-				}
-			}
-			reverse(v2.begin(),v2.end());
-			for(int i=0;i<v2.size();i++){
-			if(v2[i]>=10){
-				cout<<arr[v2[i]-10]<<"";
-			}
-			else{
-				cout<<v2[i]<<"";
-			}
-			}
+				while(x>0){
+					int y=x%m;
+					x=x/m;
+					if(y>=10){
+						v2.push_back(y);
+					} 
+					else{
+						v2.push_back(y);
+					} 
+				} // end while
+				reverse(v2.begin(),v2.end());
+				for(int i=0;i<v2.size();i++){
+					if(v2[i]>=10){
+						cout<<arr[v2[i]-10]<<"";
+					}
+					else{
+						cout<<v2[i]<<"";
+					}
+				} //end for
 			
-		
-		cout<<""<<endl;
-			}
+				cout<<""<<endl;
+			} // end if
 	
-			
-		}
+		} // end else
 		
-		
-	}
+	} // end for
 	return 0;
-}
+} // end main
